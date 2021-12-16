21eab5a2118e85f176ca575145335a108e5c7fae
jonathan andersone <jjanderson1228@gmail.com>
2021-12-16 17:53:50 -0500

    2nd commit, FrontEnd Development

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// parsing html data for POST request (when user enters video ID, grabs data from front end while on the backend)
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+
 // server loading..
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
