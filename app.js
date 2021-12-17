43f2644ff485c639cfc3706f5066bb14d076f672
jonathan andersone <jjanderson1228@gmail.com>
2021-12-16 19:41:02 -0500

    API functionality

--- app.js
+++ app.js
@@ -25,7 +25,28 @@ app.get("/", (req, res) => {
 
 app.post("/convert-mp3", async (req, res) => {
 const videoID = req.body.videoID;
-console.log(videoID)
+if (
+    videoID === undefined ||
+    videoID === "" ||
+    videoID === null
+){
+    return res.render("index", {success : false, message : "Please enter a video ID"});
+}else {
+    const fetchAPI = await fetch (`https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}` , {
+        "method" : "GET" ,
+        "headers": {
+            "x-rapidapi-key" : process.env.API_KEY,
+            "x-rapidapi-host" : process.env.API_HOST
+        }
+    });
+
+    const fetchResponse = await fetchAPI.json();
+
+    if(fetchResponse.status === "okay")
+    return res.render("index", {success : true, song_title: fetchResponse.title, song_link : fetchResponse.link});
+    else
+        return res.render("index", {success: false, message : fetchResponse.msg})
+}
 })
 
 
